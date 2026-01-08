c4d09f061d3838814c148549a735b6cc2758b023
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2026-01-08 16:00:09 +0530

    Add files via upload

---
+++ client/src/page/admin/pannel.js
@@ -0,0 +1,20 @@
+import React, {useEffect} from 'react';
+import { useLocation } from "react-router-dom";
+
+const Pannel = () => {
+  const location = useLocation();
+  const getCurrPath = sessionStorage.getItem("currentUrl");
+  useEffect(()=> {
+    if(!getCurrPath) {
+      sessionStorage.setItem("currentUrl", location.pathname);
+      sessionStorage.setItem('perviousUrl', undefined);
+    } else if(location.pathname !== getCurrPath) {
+      sessionStorage.setItem("currentUrl", location.pathname);
+      sessionStorage.setItem('perviousUrl', getCurrPath);
+    }
+  }, []);
+ return (
+    <div>Not available Now</div>
+  )
+}
+export default Pannel;
