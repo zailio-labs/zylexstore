46498fca12e673365cfc34920171e19584d6ddc9
zylexstore <zylexstore58@gmail.com>
2026-01-25 14:06:18 +0530

    Allow live content to work without API token
    Remove the check for SANITY_API_READ_TOKEN presence.

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,10 +4,8 @@
 import { defineLive } from "next-sanity/live";
 import { client } from "./client";
 
+// Token is optional - live content will work without it, but draft mode won't
 const token = process.env.SANITY_API_READ_TOKEN;
-if (!token) {
-  throw new Error("SANITY_API_READ_TOKEN is not set");
-}
 
 export const { sanityFetch, SanityLive } = defineLive({
   client,
