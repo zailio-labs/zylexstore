a6883d0ab0daf88aaee8dd646c2760dc2e9deba4
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2026-01-29 17:01:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const { PORT, MONGODB_URI, FRONTEND_URL, RATE_LIMIT_WINDOW_MS, RATE_LIMIT_MAX_REQUESTS } = require("./config");
+const { PORT, MONGODB_URI, FRONTEND_URL, RATE_LIMIT_WINDOW_MS, RATE_LIMIT_MAX_REQUESTS, EMAIL_USER } = require("./config");
 // Import routes
 const authRoutes = require('./routes/authRoutes');
 
